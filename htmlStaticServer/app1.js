1139d1cbf8b8d6e8112215840f17dd5bdab671e4
wangwenlin <wangwenlin@innotechx.com>
2019-03-01 17:25:28 +0800

    update 乱七八糟

--- htmlStaticServer/app1.js
+++ htmlStaticServer/app1.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static('/Users/wangwenlin/wwl/wwlin/index/'));
+// app.use(express.static('/Users/wangwenlin/wwl/wwlin/server.js'));
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
-var server = app.listen(80, function() {
+var server = app.listen(3338, function() {
     var host = server.address().address;
     var port = server.address().port;
 
---
+++ server.js
@@ -0,0 +1,2 @@
+console.log(123)
+return 123
